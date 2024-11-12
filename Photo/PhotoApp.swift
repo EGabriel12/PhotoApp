766c3640b76477cc2dc852cbee5a960842091d77
Elias Gabriel <eliasgab@motorola.com>
2024-11-12 13:46:01 -0400

    Initial Commit

---
+++ Photo/PhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoApp.swift
+//  Photo
+//
+//  Created by Elias Gabriel dos Santos Correa on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
